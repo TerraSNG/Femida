576cc527c30af5afb95faa86631e0d6502c765d2
WoodMan <85753549+nordbearbotdev@users.noreply.github.com>
2022-04-13 16:05:54 +0300

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿// Версия 0.1
+// Название: Femida
+// Описание: Femida - это античит для игры террария предназначенный для администраторов серверов.
+
+
+// ███████╗███████╗███╗   ███╗██╗██████╗  █████╗ 
+// ██╔════╝██╔════╝████╗ ████║██║██╔══██╗██╔══██╗
+// █████╗  █████╗  ██╔████╔██║██║██║  ██║███████║   By WoodMan with <3
+// ██╔══╝  ██╔══╝  ██║╚██╔╝██║██║██║  ██║██╔══██║
+// ██║     ███████╗██║ ╚═╝ ██║██║██████╔╝██║  ██║
+// ╚═╝     ╚══════╝╚═╝     ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝
+
+
+// Модули
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("Femida")] // Название
+[assembly: AssemblyDescription("")] // Описание
+[assembly: AssemblyConfiguration("")] //Конфиг
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Femida")] // Продукт
+[assembly: AssemblyCopyright("Copyright ©  2022")] //Копирайт (подробнее в licence.md)
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0D35456E-42DA-418B-87D4-55E32B8E1373}")] 
+
+[assembly: AssemblyVersion("1.0.1")] // Версия
+[assembly: AssemblyFileVersion("1.0.1")] // Версия файла
+[assembly: NeutralResourcesLanguageAttribute("en-RU")] // Язык
