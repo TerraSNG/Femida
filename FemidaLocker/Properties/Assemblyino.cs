dd2b37e96b42f929b6596ef1cbc42af01d8cc8e2
WoodMan <85753549+nordbearbotdev@users.noreply.github.com>
2022-04-14 13:58:02 +0300

    Create Assemblyino.cs

---
+++ FemidaLocker/Properties/Assemblyino.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+
+[assembly: AssemblyTitle("FemidaLocker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FemidaLocker")]
+[assembly: AssemblyCopyright("Copyright © TerraSNG 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1079c4d5-f263-44ac-a361-b88746780c18")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
