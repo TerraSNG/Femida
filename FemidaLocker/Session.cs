5d4b3e9cf51b67ee85d29d2937a2278135d7de81
WoodMan <85753549+nordbearbotdev@users.noreply.github.com>
2022-04-14 15:34:32 +0300

    Create Session.cs

---
+++ FemidaLocker/Session.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace FemidaLocker
+
+
+// ███████ ███████ ███    ███ ██ ██████   █████         
+// ██      ██      ████  ████ ██ ██   ██ ██   ██        
+// █████   █████   ██ ████ ██ ██ ██   ██ ███████        
+// ██      ██      ██  ██  ██ ██ ██   ██ ██   ██        
+// ██      ███████ ██      ██ ██ ██████  ██   ██        
+//                                                     made by Woodman and tSNG with <3
+//                                                     
+// ██       ██████   ██████ ██   ██ ███████ ██████      
+// ██      ██    ██ ██      ██  ██  ██      ██   ██     
+// ██      ██    ██ ██      █████   █████   ██████      
+// ██      ██    ██ ██      ██  ██  ██      ██   ██     
+// ███████  ██████   ██████ ██   ██ ███████ ██   ██     
+                                                     
+                                                     
+
+}
